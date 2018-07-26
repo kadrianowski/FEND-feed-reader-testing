855e0c6532b6de11359e923d43ea5b92eb9bb4a1
Kamil Adrianowski <kamil@e-strona.net>
2018-07-26 12:11:30 +0200

    add initial entries test

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -59,7 +59,7 @@ $(function () {
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
-        it('is hidden', function (){
+        it('is hidden', function () {
             const body = document.querySelector('body');
             expect(body.classList.contains('menu-hidden')).toBe(true);
         });
@@ -68,7 +68,7 @@ $(function () {
          * should have two expectations: does the menu display when
          * clicked and does it hide when clicked again.
          */
-        it('toggles on/off', function (){
+        it('toggles on/off', function () {
             const body = document.querySelector('body');
             const menu = document.querySelector('.menu-icon-link');
             menu.click();
@@ -78,14 +78,21 @@ $(function () {
         })
     });
     /* TODO: Write a new test suite named "Initial Entries" */
-
-    /* TODO: Write a test that ensures when the loadFeed
-     * function is called and completes its work, there is at least
-     * a single .entry element within the .feed container.
-     * Remember, loadFeed() is asynchronous so this test will require
-     * the use of Jasmine's beforeEach and asynchronous done() function.
-     */
-
+    describe('Initial Entries', function () {
+        /* TODO: Write a test that ensures when the loadFeed
+         * function is called and completes its work, there is at least
+         * a single .entry element within the .feed container.
+         * Remember, loadFeed() is asynchronous so this test will require
+         * the use of Jasmine's beforeEach and asynchronous done() function.
+         */
+        beforeEach(function(done){
+            loadFeed(0, done);
+        });
+        it('completes its work', function (){
+            const feed = document.querySelector('.feed');
+            expect(feed.children.length > 0).toBe(true);
+        });
+    });
     /* TODO: Write a new test suite named "New Feed Selection" */
 
     /* TODO: Write a test that ensures when a new feed is loaded
